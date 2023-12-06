643dc8b020751c7484007e1496ffd0f2dbc54cce
SAMYOK99 <130301448+SAMYOK99@users.noreply.github.com>
2023-12-06 23:07:54 +0545

    initial commit

---
+++ lib/homeScreen/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AdminHomeScreen extends StatefulWidget {
+  const AdminHomeScreen({super.key});
+
+  @override
+  State<AdminHomeScreen> createState() => _AdminHomeScreenState();
+}
+
+class _AdminHomeScreenState extends State<AdminHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("This is home screen"),
+      ),
+      body: Text("This is body"),
+    );
+  }
+}
