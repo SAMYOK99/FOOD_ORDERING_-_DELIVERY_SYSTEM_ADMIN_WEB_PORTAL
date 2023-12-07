60801213ca2b8bdad9658091250ad0b47467abec
SAMYOK99 <130301448+SAMYOK99@users.noreply.github.com>
2023-12-07 16:19:04 +0545

    current date and time is added

--- lib/homeScreen/home_screen.dart
+++ lib/homeScreen/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class AdminHomeScreen extends StatefulWidget {
   const AdminHomeScreen({super.key});
@@ -8,13 +11,67 @@ class AdminHomeScreen extends StatefulWidget {
 }
 
 class _AdminHomeScreenState extends State<AdminHomeScreen> {
+  String timeString = "";
+  String dateString = "";
+  String formatCurrentTime(DateTime time){
+    return DateFormat("hh:mm:ss a").format(time);
+  }
+  String formatCurrentDate(DateTime date){
+    return DateFormat("yyyy, dd MMMM").format(date);
+  }
+  getCurrentTime(){
+    final DateTime timeNow = DateTime.now();
+    final String liveTime = formatCurrentTime(timeNow);
+    final String liveDate = formatCurrentDate(timeNow);
+
+    if(this.mounted){
+      setState(() {
+        timeString = liveTime;
+        dateString = liveDate;
+      });
+    }
+  }
+  @override
+  void initState() {
+    super.initState();
+    //date
+    formatCurrentDate(DateTime.now());
+    //time
+    formatCurrentTime(DateTime.now());
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      getCurrentTime();
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("This is home screen"),
       ),
-      body: Text("This is body"),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              timeString,
+              style: TextStyle(
+                fontSize: 20 ,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 3
+              ),
+            ),
+            Text(
+              dateString,
+              style: TextStyle(
+                fontSize: 20 ,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 3
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
